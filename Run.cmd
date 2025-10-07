0e57104072a7c82eebf91f09bf347dd599a0ecb8
Muhammad Huzaifa Hussain <143995861+MHuzaifaHussain@users.noreply.github.com>
2025-10-07 07:55:56 +0500

    Update Run.cmd

--- Run.cmd
+++ Run.cmd
@@ -17,7 +17,7 @@ REM You can install it by running: npm install -g concurrently
 
 REM The command below uses 'concurrently' to run both the backend and frontend commands.
 REM It now activates the venv in the Backend before starting the server.
-concurrently --names "BACKEND,FRONTEND" -c "blue,green" "cd Backend && venv48\Scripts\activate && uvicorn app.main:app --host 127.0.0.1 --port 8000" "cd Frontend\pneumo-detect-frontend && npm run dev"
+concurrently --names "BACKEND,FRONTEND" -c "blue,green" "cd Backend && venv\Scripts\activate && uvicorn app.main:app --host 127.0.0.1 --port 8000" "cd Frontend\pneumo-detect-frontend && npm run dev"
 
 echo.
 echo Servers have been stopped.
